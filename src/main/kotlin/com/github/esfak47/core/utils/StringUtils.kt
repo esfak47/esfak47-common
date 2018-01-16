003da3c4304f62b3147c5219750e6c0f9e01537a
esfak47 <esfak47@gmail.com>
2018-01-16 17:31:04 +0800

    fix final

--- src/main/kotlin/com/github/esfak47/core/utils/StringUtils.kt
+++ src/main/kotlin/com/github/esfak47/core/utils/StringUtils.kt
@@ -4,7 +4,7 @@ package com.github.esfak47.core.utils
  * @author Tony
  * * Created by tony on 2018/1/16.
  */
-class StringUtils {
+object StringUtils {
     fun isEmpty(string: String?): Boolean {
         return string != null && "" == string.trim { it <= ' ' }
     }
